482735f67e495231d0d9ee43856ca01c632cedb4
Serge Kalala <tchebo2000@yahoo.fr>
2026-02-19 15:47:57 -0500

    Use DATABASE_URL from environment

--- app/db.py
+++ app/db.py
@@ -6,9 +6,9 @@ from sqlalchemy import create_engine
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
 
-DATABASE_URL = "postgresql://postgres:Oceane.15216556%24@localhost:5432/ai_frontdesk?client_encoding=utf8"
+DATABASE_URL = os.getenv("DATABASE_URL")
 
-engine = create_engine(DATABASE_URL, pool_pre_ping=True, pool_recycle=3600, connect_args={"options": "-c client_encoding=utf8"})
+engine = create_engine(DATABASE_URL, pool_pre_ping=True, pool_recycle=3600)
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
 
 Base = declarative_base()
